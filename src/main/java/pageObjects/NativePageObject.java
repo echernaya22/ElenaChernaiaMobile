9d8e3ce4542200ba72bb9606ea917240581ecf29
MSI\echernaya <echernaya22@yandex.ru>
2021-06-22 14:32:00 +0300

    add second HW

--- src/main/java/pageObjects/NativePageObject.java
+++ src/main/java/pageObjects/NativePageObject.java
@@ -8,8 +8,30 @@ import org.openqa.selenium.support.PageFactory;
 
 public class NativePageObject  {
 
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
+    WebElement loginEmailField;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
+    WebElement loginPasswordField;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
-    WebElement signInBtn;
+    WebElement signInButton;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
+    WebElement loginRegisterButton;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
+    WebElement registerEmailField;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
+    WebElement registerUsernameField;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
+    WebElement registerPasswordField;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
+    WebElement registerConfirmPasswordField;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
+    WebElement registerButton;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
+            + "/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]"
+            + "/android.view.ViewGroup/android.widget.TextView")
+    WebElement budgetTitle;
 
     public NativePageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements( new AppiumFieldDecorator(appiumDriver), this);
--- src/main/java/pageObjects/WebPageObject.java
+++ src/main/java/pageObjects/WebPageObject.java
@@ -1,13 +1,21 @@
 package pageObjects;
 
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class WebPageObject  {
 
+    @FindBy(xpath = "//input[@name='q']")
+    WebElement inputSearchField;
+    @FindBy(xpath = "//*[@id='rso']//div[@role='heading'][@aria-level='3']")
+    List<WebElement> searchResultList;
+
     public WebPageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements(appiumDriver, this);
-
     }
 
 
